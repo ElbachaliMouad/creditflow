d94912aa9827c1622be96cf584c4284e43882aba
MOUAD ELBACHALI <ELBACHALIMOUAD@GMAIL.COM>
2025-06-12 10:15:56 +0100

    set up

---
+++ ligispluserver/src/main/java/com/example/ligisplus/LigisplusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ligisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigisplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigisplusApplication.class, args);
+	}
+
+}
---
+++ ligispluserver/src/test/java/com/example/ligisplus/LigisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ligisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigisplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
